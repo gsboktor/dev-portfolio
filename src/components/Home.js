949e79e3e5ad2b5514756a61631ca208c59ac99d
George Boktor <georgeboktor@Georges-MacBook-16.local>
2021-05-05 17:07:13 -0500

    NavBar and home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react"
+import image from "../bg_img.jpeg";
 
 export default function Home(){
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="img" className="absolute object-cover w-full h-full"/>
+            <section className ="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">George Boktor - Software Engineer</h1>
+            </section>
+        </main>
+    )
 }
